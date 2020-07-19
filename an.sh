09d7969415cf6b8fe0b05a23a0799a820378083f
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2020-07-19 13:05:14 +0000

    Add files via upload

---
+++ an.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo -e "\n"
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+echo -e "\n"
+
+source ~/.nvm/nvm.sh
+
+echo -e "\n"
+
+nvm --version
+
+echo -e "\n"
+
+nvm install node
+
+echo -e "\n"
+
+nvm use node
+
+echo -e "\n"
+
+nvm install v10.16.3
+
+echo -e "\n"
+
+nvm use v10.16.3
+
+echo -e "\n"
+
+nvm alias default v10.16.3
+
+echo -e "\n"
+
+node -v
+
+echo -e "\n"
+
+source ~/.bashrc
+
+echo -e "\n"
