f8934e3d10d950e145455f6d53421aa6b6b18908
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2020-07-20 17:48:18 +0530

    Update an.sh

--- an.sh
+++ an.sh
@@ -29,7 +29,7 @@ node -v
 
 echo -e "\n"
 
-export PATH=$PATH:/root/.nvm/versions/node/v10.16.3/bin/node/ >> ~/.bashrc
+export NVM_DIR="/root/.nvm"
 
 echo -e "\n"
 
@@ -40,14 +40,8 @@ echo $PATH
 which node
 node -v
 whereis node
-export PATH=$PATH:/root/.nvm/versions/node/v10.16.3/bin/node/ >> ~/.bashrc
-#export PATH=${PATH}:/root/.nvm/versions/node/v10.16.3/bin
-#export PATH="$PATH:/root/.nvm/versions/node/v10.16.3/bin/node/" >> ~/.bash_aliases
-#export PATH="/usr/local/bin:$PATH"
-#export PATH="/usr/local/bin:$PATH" 
-#$export PATH="$/root/.nvm/versions/node/v10.16.3/bin:$PATH"
-#export PATH=$PATH:/root/.nvm/versions/node/v10.16.3/bin/node
-#export PATH="$PATH:/root/.nvm/versions/node/v10.16.3/bin/node"
-#export PATH="/usr/local/bin/node:$PATH"
-#export PATH="$/root/.nvm/versions/node/v10.16.3/bin/node:$PATH"
-
+export NVM_DIR="/root/.nvm"
+which node
+node -v
+whereis node
+#export PATH=$PATH:/root/.nvm/versions/node/v10.16.3/bin/node/ >> ~/.bashrc
