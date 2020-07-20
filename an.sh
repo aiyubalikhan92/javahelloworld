9f3673740593664c0b2993b77d122f8abf3daa39
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2020-07-20 14:18:44 +0530

    Update an.sh

--- an.sh
+++ an.sh
@@ -51,6 +51,5 @@ export PATH="$PATH:/usr/local/bin/node"
 #PATH=/usr/bin/node:$PATH
 #export PATH="/root/.nvm/versions/node/v10.16.3/bin/npm:/usr/local/bin/node:/usr/local/sbin:~/bin:$PATH"
 #export PATH="/root/.nvm/versions/node/v10.16.3/bin/npm:$PATH"
-which npm
 #export PATH="/usr/local/bin/npm:$PATH" 
 #export PATH="$/root/.nvm/versions/node/v10.16.3/bin/npm:$PATH"
